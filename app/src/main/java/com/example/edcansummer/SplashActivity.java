324f7aec160ffb3f7f6901cea0dffcac19ddd897
지민경 <63912449+jjhair119@users.noreply.github.com>
2020-08-10 12:40:19 +0900

    EDCAN Summer Project 0810

---
+++ app/src/main/java/com/example/edcansummer/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.edcansummer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }, 1500);
+    }
+}
